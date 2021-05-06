c1dba6ca3395c8667d1cc45b00a033a9bb40d279
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2021-05-06 12:07:20 +0200

    Delete gpu-monitor directory

--- gpu-monitor/error.py
+++
@@ -1,8 +0,0 @@
-ERRORLOG='./logs/error.log'
-# should never occurr...
-def errorLog(sentence):
-    f=open(ERRORLOG, 'a+')
-    f.write("ERROR: "+sentence)
-    f.write("\n")
-    f.flush()
-    f.close()
